7112c48eacfde1e776e4028eccb05b13be829040
213500959@stu.ukzn.ac.za <213500959@stu.ukzn.ac.za>
2015-03-10 14:40:16 +0200

    Methods

---
+++ Project2/TicTacToe.cpp
@@ -0,0 +1,48 @@
+#include "TicTacToe.h"
+#include<iostream>
+using namespace std;
+
+TicTacToe::TicTacToe()
+{
+}
+
+
+TicTacToe::~TicTacToe()
+{
+}
+
+
+
+/*
+void restart(int flag);
+void move(int pos);
+void print();
+bool isWon(void);
+*/
+
+void TicTacToe::restart(int flag)
+{
+	
+}//restart
+
+void TicTacToe::move(int pos)
+{
+
+}//move
+
+void TicTacToe::print()
+{
+
+}//print
+
+bool TicTacToe::isWon(void)
+{
+	return true;
+
+}//isWon
+
+
+void TicTacToe::makeBoard(void)
+{
+
+}//makeboard
